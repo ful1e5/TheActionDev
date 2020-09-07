37bdf9304f4979be1eb04aa162551c2c6c1a4eb7
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2020-09-07 12:10:00 +0530

    💅 Prettier

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -21,7 +21,7 @@ test("test runs", () => {
   process.env["INPUT_MILLISECONDS"] = "500";
   const ip = path.join(__dirname, "..", "lib", "main.js");
   const options: cp.ExecSyncOptions = {
-    env: process.env,
+    env: process.env
   };
   console.log(cp.execSync(`node ${ip}`, options).toString());
 });
