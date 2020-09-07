b79c9a3ef9e91d09533411040c4d6c3829c58eb5
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2020-09-07 17:46:48 +0530

    🔐 auth

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,10 @@
-import * as core from "@actions/core";
-import { wait } from "./utils/wait";
+import core from "@actions/core";
+import { authenticate } from "./utils/auth";
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput("milliseconds");
-    core.debug(`Waiting ${ms} milliseconds ...`); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-
-    core.debug(new Date().toTimeString());
-    await wait(parseInt(ms, 200));
-    core.debug(new Date().toTimeString());
-
-    core.setOutput("time", new Date().toTimeString());
+    const token: string = core.getInput("token");
+    authenticate(token);
   } catch (error) {
     core.setFailed(error.message);
   }
