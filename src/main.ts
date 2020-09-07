fb2cf263fcd9327e6997f479b11c3a16f22e45a3
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2020-09-07 17:55:33 +0530

    🔐 logging auth status

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
-import core from "@actions/core";
+import core, { debug } from "@actions/core";
 import { authenticate } from "./utils/auth";
 
 async function run(): Promise<void> {
   try {
     const token: string = core.getInput("token");
-    authenticate(token);
+    const isAuth = authenticate(token);
+    debug(`I'm authenticated : ${isAuth}`);
   } catch (error) {
     core.setFailed(error.message);
   }
