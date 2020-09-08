deb83ed2450473d7cd40739c2086c37887697e75
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2020-09-08 08:18:29 +0530

    🚨 Error

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -3,15 +3,11 @@ import { debug } from "@actions/core";
 const isAuth = false;
 
 export async function authenticate(token: string): Promise<boolean> {
-  return new Promise(resolve => {
-    try {
-      debug(token);
-      resolve(isAuth);
-      if (token === "") {
-        throw new Error("Invalid Token");
-      }
-    } catch (error) {
-      console.error(error);
+  return new Promise((resolve, reject) => {
+    debug(token);
+    resolve(isAuth);
+    if (token === "") {
+      reject(new Error("Token not be empty"));
     }
   });
 }
