bbef8eea75d96f86bc53d6faa947d95e702ad832
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2020-09-08 08:11:30 +0530

    🚨Auth Error Handling

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -4,7 +4,14 @@ const isAuth = false;
 
 export async function authenticate(token: string): Promise<boolean> {
   return new Promise(resolve => {
-    debug(token);
-    resolve(isAuth);
+    try {
+      debug(token);
+      resolve(isAuth);
+      if (token === "") {
+        throw new Error("Invalid Token");
+      }
+    } catch (error) {
+      console.error(error);
+    }
   });
 }
