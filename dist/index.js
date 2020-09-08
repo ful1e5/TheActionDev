a6134c8a391261720dbbe4bb047b1370713ce8b2
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2020-09-08 08:11:56 +0530

    ⚡ Build with auth error handling

--- dist/index.js
+++ dist/index.js
@@ -7,6 +7,25 @@ require('./sourcemap-register.js');module.exports =
 /***/ (function(__unused_webpack_module, exports, __webpack_require__) {
 
 
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -16,17 +35,15 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", ({ value: true }));
-const core_1 = __importDefault(__webpack_require__(186));
+const core_1 = __importStar(__webpack_require__(186));
 const auth_1 = __webpack_require__(751);
 function run() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
             const token = core_1.default.getInput("token");
-            auth_1.authenticate(token);
+            const isAuth = auth_1.authenticate(token);
+            core_1.debug(`I'm authenticated : ${isAuth}`);
         }
         catch (error) {
             core_1.default.setFailed(error.message);
@@ -58,8 +75,16 @@ const isAuth = false;
 function authenticate(token) {
     return __awaiter(this, void 0, void 0, function* () {
         return new Promise(resolve => {
-            core_1.debug(token);
-            resolve(isAuth);
+            try {
+                core_1.debug(token);
+                resolve(isAuth);
+                if (token === "") {
+                    throw new Error("Invalid Token");
+                }
+            }
+            catch (error) {
+                console.error(error);
+            }
         });
     });
 }
