37b49ba6ff92aea8aa145b2afd18211ccb16d868
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2020-09-08 18:11:22 +0530

    ⚡ Build with DevApi

--- dist/index.js
+++ dist/index.js
@@ -38,71 +38,15 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.run = void 0;
 const core = __importStar(__webpack_require__(186));
-const auth_1 = __webpack_require__(751);
 exports.run = () => __awaiter(void 0, void 0, void 0, function* () {
     try {
-        const token = core.getInput("token", { required: true });
-        yield auth_1.authenticate(token);
+        // const token: string = core.getInput("token", { required: true });
     }
     catch (error) {
         core.setFailed(error.message);
     }
 });
 exports.run();
-
-
-/***/ }),
-
-/***/ 751:
-/***/ (function(__unused_webpack_module, exports, __webpack_require__) {
-
-
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-Object.defineProperty(exports, "__esModule", ({ value: true }));
-exports.authenticate = void 0;
-const core = __importStar(__webpack_require__(186));
-const isAuth = false;
-/**
- *
- * @param token dev.to api token
- *
- */
-exports.authenticate = (token) => __awaiter(void 0, void 0, void 0, function* () {
-    return new Promise((resolve, reject) => {
-        core.info(token);
-        resolve(isAuth);
-        if (token === "") {
-            reject(new Error("Token not be empty"));
-        }
-    });
-});
 
 
 /***/ }),
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,8 @@
 import * as core from "@actions/core";
-import { authenticate } from "./utils/auth";
 
 export const run = async (): Promise<void> => {
   try {
-    const token: string = core.getInput("token", { required: true });
-    await authenticate(token);
+    // const token: string = core.getInput("token", { required: true });
   } catch (error) {
     core.setFailed(error.message);
   }
