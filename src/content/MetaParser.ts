9744a44651365ea731e6acc1344f273049f44aa8
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2020-09-09 11:59:54 +0530

    🗄️ Utils for parsing .yml metadata

---
+++ src/content/MetaParser.ts
@@ -0,0 +1,25 @@
+export function titleParser(markdown: string) {
+  const yaml = markdown.match(/^\s*\-{3}\n([\s\S]*?)\n\-{3}/);
+  if (!yaml) {
+    return null;
+  }
+  const title = yaml[1].match(/^[ \t]*title:[ \t]*(.*?)[ \t]*$/m);
+  if (!title) {
+    return null;
+  }
+
+  return decodeURIComponent(title[1]);
+}
+
+export function publishStateParser(markdown: string) {
+  const yaml = markdown.match(/^\s*\-{3}\n([\s\S]*?)\n\-{3}/);
+  if (!yaml) {
+    return false;
+  }
+  const published = yaml[1].match(/^[ \t]*published:[ \t]*(.*?)[ \t]*$/m);
+  if (!published) {
+    return false;
+  }
+
+  return published[1] === "true";
+}
