ff4fb400b0985e3c8494bd3232e7253b512202e6
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2020-09-09 12:00:20 +0530

    🗃  sync local articles init

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
 import * as core from "@actions/core";
 import { DevAPI } from "./api/DevApi";
+import { RepoArticlesProvider } from "./content/RepoArticlesProvider";
 
 export const run = async (): Promise<void> => {
   try {
     const apiKey: string = core.getInput("apiKey", { required: true });
+    const articlesDir: string = core.getInput("articles-directory");
 
     const api = new DevAPI(apiKey);
+    const repo = new RepoArticlesProvider(articlesDir);
+
+    repo.syncArticles();
     const lists = await api.list();
     console.log(lists);
   } catch (error) {
