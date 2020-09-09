e76e3fb9a0249b2af618dcf6bb4055aef2b5e227
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2020-09-09 09:09:33 +0530

    ⭐ list all articles

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import * as core from "@actions/core";
+import { DevAPI } from "./api/DevApi";
 
 export const run = async (): Promise<void> => {
   try {
-    // const token: string = core.getInput("token", { required: true });
+    const token: string = core.getInput("token", { required: true });
+
+    const api = new DevAPI(token);
+    const lists = await api.list();
+    console.log(lists);
   } catch (error) {
     core.setFailed(error.message);
   }
