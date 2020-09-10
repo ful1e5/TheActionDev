026af374a72482ca57f5a357048d46cf61b5b88c
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2020-09-10 08:00:04 +0530

    🙈 ignoring Default Git Docs Files with input

--- src/content/RepoArticlesProvider.ts
+++ src/content/RepoArticlesProvider.ts
@@ -1,14 +1,34 @@
 import * as glob from "@actions/glob";
+import * as core from "@actions/core";
+
 export class RepoArticlesProvider {
+  private _excludePattern: string[] = [
+    "!**/README.md",
+    "!**/CONTRIBUTING.md",
+    "!**/CODE_OF_CONDUCT.md",
+    "!**/CHANGELOGS.md"
+  ];
+
+  constructor(private _path?: string) {
+    this._excludePattern.push(
+      ...core
+        .getInput("ignoreFiles")
+        .split(",")
+        .map(f => `!${this._path}/${f}`)
+    );
+  }
+
   private async files() {
-    const patterns = ["**/dev.md"];
-    const globber = await glob.create(patterns.join("\n"));
+    const pattern = [`${this._path}/*.md`, this._excludePattern];
+    const globber = await glob.create(pattern.join("\n"), {
+      followSymbolicLinks: false
+    });
     return await globber.glob();
   }
 
   async sync() {
-    // TODO
+    // TODO: Sync with Cron job
     console.log(await this.files());
-    console.log("snying..");
+    console.log("snycing..");
   }
 }
