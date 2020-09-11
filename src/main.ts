a54a2b6015a23214006dbc9393c07aef85406977
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2020-09-11 13:13:15 +0530

    ⚡ Use LowDBApi

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
+import path from "path";
 import * as core from "@actions/core";
+import { LowDBApi } from "./api/LowDBApi";
 import { DevAPI } from "./api/DevApi";
 import { RepoArticlesProvider } from "./content/RepoArticlesProvider";
 
@@ -6,13 +8,16 @@ export const run = async (): Promise<void> => {
   try {
     const apiKey: string = core.getInput("api-key", { required: true });
     const articlesPath: string = core.getInput("directory");
+    const dbPath = path.resolve(process.cwd(), "sync.json");
 
+    const db = new LowDBApi(dbPath);
     const api = new DevAPI(apiKey);
+    const repo = new RepoArticlesProvider(articlesPath);
+
     const articles = await api.list();
     const authorProfileLink = await api.profileLink();
 
-    const repo = new RepoArticlesProvider(articlesPath);
-    repo.sync(articles, authorProfileLink);
+    repo.sync({ articles, authorProfileLink, db });
   } catch (error) {
     core.setFailed(error.message);
   }
