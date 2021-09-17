77ace50a7c40cb0ca1e84c1f97294b1d08d67513
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2021-09-17 17:54:24 +0530

    cleanup old mess

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,3 @@
-import * as core from "@actions/core";
+export const main = async (): Promise<void> => {};
 
-import { DevAPI } from "./api/DevApi";
-import { RepoArticlesProvider } from "./content/RepoArticlesProvider";
-
-/**
- * Run TheActionDev
- */
-export const run = async (): Promise<void> => {
-  try {
-    const apiKey: string = core.getInput("api-key", { required: true });
-    const articlesPath: string = core.getInput("directory");
-
-    const api = new DevAPI(apiKey);
-    const repo = new RepoArticlesProvider(articlesPath);
-
-    repo.sync(api);
-  } catch (error: any) {
-    core.setFailed(error.message);
-  }
-};
-
-run();
+main();
