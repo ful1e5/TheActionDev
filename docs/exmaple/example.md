a24a57a58498ddca3c89b9cbc343ba3850fd51d1
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2022-05-30 16:54:36 +0530

    config refactor for ci

--- docs/exmaple/example.md
+++ docs/exmaple/example.md
@@ -3,7 +3,9 @@ title: New Post
 published: false
 description: New Post
 series: TheActionDev
-tags: typescript, javascript
+tags:
+  - typescript
+  - javascript
 cover_image: https://imgur.com/67WlRoB.png
 ---
 
---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.export = {
+  clearMocks: true,
+  moduleFileExtensions: ["js", "ts"],
+  testEnvironment: "node",
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(ts)$",
+  collectCoverageFrom: ["<rootDir>/**/*.ts"],
+  roots: ["<rootDir>"],
+  transform: {
+    "^.+\\.ts$": "ts-jest"
+  },
+  verbose: true
+};
